377defe8048588bafe64f35b9a2a7fc09daee73e
pavel <PavelFomBe;@yandex.ru>
2020-12-20 03:59:03 +0300

    Patched login form

--- src/main/java/login/LoginServlet.java
+++ src/main/java/login/LoginServlet.java
@@ -4,13 +4,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            req.getRequestDispatcher("/html/login/login_default.html").include(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
-        printWriter.println("Sosi pisku");
+        printWriter.println("<b1>Skin' tomu na kogo podrochil</b1>");
     }
 }
--- src/main/java/mainServlet/MainServlet.java
+++ src/main/java/mainServlet/MainServlet.java
@@ -1,5 +1,7 @@
 package mainServlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,15 +13,9 @@ import java.io.PrintWriter;
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        PrintWriter printWriter = resp.getWriter();
-//        printWriter.println("Sosi pisku");
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        HttpSession session = req.getSession(false);
-        boolean resetParam = false;
-        if (session == null) {
-            req.getRequestDispatcher("html/login/login_default.html").include(req, resp);
-            out.println("<br>Login to make a post");
+        if(req.getSession(false) == null) {
+            String path = req.getContextPath() + "/schedule/login/";
+            resp.sendRedirect(path);
         }
     }
 }
